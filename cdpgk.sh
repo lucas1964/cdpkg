3025be8df915f89047c1f490b00e4b3c9e566759
lucas1964 <luigi.cassolini@gmail.com>
2024-01-09 13:21:35 +0100

    Create cdpgk.sh
    First release

---
+++ cdpgk.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+shopt -q
+#
+#******************************************************
+#
+# Per il check su nuove distribuzioni (p.e. Mint) agguingere il nome dell'array di comptenza
+# Script creato da Luigi Cassolini
+# Si consiglia di posizionare lo script in /opt/update/
+#
+#*******************************************************
+#
+declare -a REDHAT_LIKE=( "Rocky" "Centos" )
+declare -a DEBIAN_LIKE=( "Debian" "Ubuntu" )
+
+DOWNLOAD="Download packages on"
+OS=$(cat /etc/*release | grep ^NAME)
+LINE="==============================================="
+update_date=$(date +%d-%m-%Y)
+update_time=$(date +%H:%M)
+
+#UPDATE per Piattaforme Red Hat Like
+for i in "${REDHAT_LIKE[@]}"
+do
+        if  echo "$OS" | grep -q $i; then
+                echo $LINE>cron_download_update.log
+                echo "Execution date time: $update_date at $update_time">>cron_download_update.log
+                echo $LINE>>cron_download_update.log
+                yum check-update>>cron_download_update.log
+                yum update --downloadonly -y
+                echo $LINE>>cron_download_update.log
+        fi
+done
+
+#UPDATE per Piattaforme Debian Like
+for i in "${DEBIAN_LIKE[@]}"
+do
+        if  echo "$OS" | grep -q $i; then
+                echo "Start at $update_date $update_time"
+                echo $LINE>cron_download_update.log
+                echo "Execution date time: $update_date at $update_time">>cron_download_update.log
+                echo $LINE>>cron_download_update.log
+                /usr/bin/apt update &> /dev/null
+                /usr/bin/apt list --upgradable>>cron_download_update.log 2> /dev/null
+                /usr/bin/apt -y -d upgrade &> /dev/null
+
+        fi
+done
+echo $LINE>>cron_download_update.log
+echo "End at   $update_date $update_time">>cron_download_update.log
+echo $LINE>>cron_download_update.log
+exit 0
